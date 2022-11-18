43564bbce827adca7f1777058d9fc1d34ff90d80
Gus <Gus@Michelles-MacBook-Pro.local>
2022-11-19 00:49:23 +0800

    updated navigation

--- frontend/src/Navigation.js
+++ frontend/src/Navigation.js
@@ -1,8 +1,15 @@
 const Navigation = () => {
     // <p> to be replaced with nav
     return (
-        <header>
-            <p>GM Logo</p>
+        <header id="head">
+            <p id="logo">GM Logo</p>
+            <nav id="nav">
+                <a className="link" href="/" >Home</a>
+                <a className="link" href="/about">About</a>
+                <a className="link" href="/contact">Contact Me</a>
+                <a className="link" href="/projects">Projects</a>
+                <a className="link" href="/resume">Resume</a>
+            </nav>
         </header>
     );
 }
