aef5cca5d47a532ecf86658c552503083470e23d
Gustavo <gustavo_roger@yahoo.com>
2023-03-22 03:13:53 -0700

    Projects page Update

--- frontend/src/places/Projects.js
+++ frontend/src/places/Projects.js
@@ -1,7 +1,17 @@
 const Projects = () => {
     return (
         <main>
-            <h1>Projects and Testimonials</h1>
+            <h1>A Glimpse Of What Is Possible</h1>
+            {/* showcase similar to home and about */}
+            <h2>Frontend</h2>
+            <p>project</p>
+            <h2>Backend</h2>
+            <p>project</p>
+            <p> 
+                Like what you see?
+                <br/> 
+                <a href='/contact' className='link'> Contact Me!</a>
+            </p>
         </main>
     );
 }
