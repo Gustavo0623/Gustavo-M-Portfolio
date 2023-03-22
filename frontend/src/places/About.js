f7a62149d6bb7950faef5865e65c8928a55ed5c3
Gustavo <gustavo_roger@yahoo.com>
2023-03-22 03:01:06 -0700

    about page update with pseudocode setting up template

--- frontend/src/places/About.js
+++ frontend/src/places/About.js
@@ -1,7 +1,13 @@
 const About = () => {
     return (
         <main>
-            <h1>About Me</h1>
+            <h1>Who Is Gustavo Martinez?</h1>
+            {/* potential showcase styling similar to home projects, to keep styling consistent */}
+            <h2>Mission</h2>
+            <p className='msg'>Mission statement</p>
+            <h2>Background</h2>
+            <p>Educational</p>
+            <p>Professional</p>
         </main>
     );
 }
