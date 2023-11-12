5922ccdea924649170755f06b560b2b04252242a
Gustavo <gustavo_roger@yahoo.com>
2023-11-12 12:58:16 -0800

    go white sd project component integration complete

---
+++ frontend/src/components/GWSD.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const GWDetails = () => {
+
+    return (
+        <div id='fitTrack' className='fade-in'>
+            <p className='msg' id='proj-role'><b className='msg role'>Digital Solutions Consultant</b></p>
+            
+            <ul className='detail-list'>
+                <li className='list-item fade-in'>Provided expert guidance and consultation to clients seeking digital solutions for their business.</li>
+                <li className='list-item fade-in'>Collaborated closely with clients to understand their specific needs and business goals.</li>
+                <li className='list-item fade-in'>Advised on effective digital strategies and solutions, with a focus on optimizing online presence.</li>
+                <li className='list-item fade-in'>Specialized in Squarespace platform, ensuring seamless website development and customization.</li>
+                <li className='list-item fade-in'>Facilitated a smooth transition to the live environment, managing domain setup and configuration.</li>
+                <li className='list-item fade-in'>Offered ongoing support and training to empower clients in managing their digital assets.</li>
+                <li className='list-item fade-in'>Ensured client satisfaction by delivering high-quality digital solutions aligned with their vision.</li>
+            </ul>
+        </div>
+    )
+}
+
+export default GWDetails
--- frontend/src/places/Projects.js
+++ frontend/src/places/Projects.js
@@ -2,11 +2,11 @@ import React, { useState, useContext } from "react";
 import FTDetails from "../components/FtDetails";
 import GoDetails from "../components/GoDetails";
 import { NavContext } from '../App';
+import GWDetails from "../components/GWSD";
 
 const Projects = () => {
 
     const { navState } = useContext(NavContext);
-    const [details, setDetails] = useState(false)
     const [front, setFront] = useState(true)
     const [project, setProject] = useState(0)
 
@@ -22,22 +22,23 @@ const Projects = () => {
                     <div id='pro-split'>
                         <div id='bg-content'>
                             {/* showcase similar to home and about */}
-                            <strong className={project === 0 ? 'drop fade-in select' : 'drop tabs fade-in'} onClick={()=>{setProject(0)}}>Fit-Track</strong>
-                            <strong className={project === 1 ? 'drop fade-in select' : 'drop tabs fade-in'} onClick={()=>{setProject(1)}}>Go-Bird-Go</strong>
+                            <b className={project === 0 ? 'drop fade-in select' : 'drop tabs fade-in'} onClick={()=>{setProject(0)}}>Go White SD</b>
+                            <b className={project === 1 ? 'drop fade-in select' : 'drop tabs fade-in'} onClick={()=>{setProject(1)}}>Fit-Track</b>
+                            <b className={project === 2 ? 'drop fade-in select' : 'drop tabs fade-in'} onClick={()=>{setProject(2)}}>Go-Bird-Go</b>
                         </div>
                         <div id='bg-display'>
                             <div id='pro-display'>
-                                { project === 0 ? <img src='./Photos/Screen Shot 2023-01-18 at 10.19.13 PM (2).jpg' alt='Selected App Screenshot' id='proj-photo' className='fade-in'/>: project === 1 ? <img src='./Photos/Screen Shot 2023-04-30 at 5.43.32 PM.png' alt='Selected App Screenshot' id='proj-photo' className="fade-in"/> : null}
+                                { project === 0 ? <img src='./Photos/GWSD.png' alt='Selected App Screenshot' id='proj-photo' className='fade-in'/> : project === 1 ? <img src='./Photos/Screen Shot 2023-01-18 at 10.19.13 PM (2).jpg' alt='Selected App Screenshot' id='proj-photo' className='fade-in'/>: project === 2 ? <img src='./Photos/Screen Shot 2023-04-30 at 5.43.32 PM.png' alt='Selected App Screenshot' id='proj-photo' className="fade-in"/> : null}
                                 <div className='separate gap fade-in'>
                                     <div className='join'>
-                                        <img src='./Photos/icons8-github-94.png' alt='place marker' className='place-mark i-grow'/>
-                                        <a href={ project === 0 ? 'https://github.com/proptechnovator/fittrack-v2' : 'https://github.com/Gustavo0623/Go-Bird-Go' } className='half proj-link msg link tab'> Code </a>
+                                        { project !== 0 ? <img src='./Photos/icons8-github-94.png' alt='place marker' className='place-mark i-grow'/> : null}
+                                        { project !== 0 ? <a href={ project === 1 ? 'https://github.com/proptechnovator/fittrack-v2' : project === 2 ? 'https://github.com/Gustavo0623/Go-Bird-Go' : null} className='half proj-link msg link tab'> Code </a> : <b className='half msg proj-link'>No Code Solution</b>}
                                     </div>
                                     <div className='half proj-link' id='live'>
-                                        { project === 0 ? <a href='https://fittrack-frontend.herokuapp.com/' className='link msg tab'>Live Demo</a> : project === 1 ? <p className="msg" id='invalid'>Demo Coming Soon!</p> : null}
+                                        { project === 0 ? <a href='https://gowhitesd.com' className='link msg tab'>Live Site</a> : project === 1 ? <a href='https://fittrack-frontend.herokuapp.com/' className='link msg tab'>Live Demo</a> : project === 2 ? <p className="msg" id='invalid'>Demo Coming Soon!</p> : null}
                                     </div>
                                 </div>
-                                { project === 0 ? <FTDetails/> : project === 1 ? <GoDetails/> : null}
+                                { project === 0 ? <GWDetails/> : project === 1 ? <FTDetails/> : project === 2 ? <GoDetails/> :null}
                             </div>
                             <p id='pro-link' className='msg fade-in'> 
                                 Like what you see?
