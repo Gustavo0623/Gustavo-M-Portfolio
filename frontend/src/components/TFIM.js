ffc40fa53b2d8d6e52a845fc169b443cd50fca88
Gustavo <gustavo_roger@yahoo.com>
2024-01-23 21:09:35 -0800

    add TFIM information. TODO: Implement component onto project page, fix link errors

---
+++ frontend/src/components/TFIM.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TFIMDetails = () => {
+
+    return (
+        <div id='fitTrack' className='fade-in'>
+            <p className='msg' id='proj-role'><b className='msg role'>Digital Solutions Consultant</b></p>
+            <p className='msg'><b className='msg role'>Skills:</b> WIX Platform Expertise, Web Design, Domain Management, Search Engine Optimization (SEO), Online Presence Optimization, Client Training, Strategic Consulting, Problem-Solving, User Experience (UX) Enhancement, Live Website Transition, Digital Solutions Tailoring, Trend Integration, and Effective Communication.</p>
+            <b className='msg role'>Key Responsibilities:</b>
+            <ul className='detail-list'>
+                <li className='list-item fade-in'>Provided expert guidance and consultation to clients seeking digital solutions for their business.</li>
+                <li className='list-item fade-in'>Collaborated closely with clients to understand their specific needs and business goals.</li>
+                <li className='list-item fade-in'>Advised on effective digital strategies and solutions, with a focus on optimizing online presence.</li>
+                <li className='list-item fade-in'>Specialized in WIX platform, ensuring seamless website development and customization.</li>
+                <li className='list-item fade-in'>Facilitated a smooth transition to the live environment, managing domain setup and configuration.</li>
+                <li className='list-item fade-in'>Offered ongoing support and training to empower clients in managing their digital assets.</li>
+                <li className='list-item fade-in'>Ensured client satisfaction by delivering high-quality digital solutions aligned with their vision.</li>
+            </ul>
+        </div>
+    )
+}
+
+export default TFIMDetails
